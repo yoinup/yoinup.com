bcac4bad1442dd51a491bbdad05d8f3c60ba5d07
Pepe Cano <ppcanodehuelva@gmail.com>
2013-04-14 13:56:51 +0200

    Add Google Play Store

--- minimal.rb
+++ minimal.rb
@@ -3,10 +3,10 @@ require 'sinatra/base'
 
 class Minimal < Sinatra::Base
   set :static, true
-  set :public, File.dirname(__FILE__) + '/'
+  set :public_dir, File.dirname(__FILE__) + '/'
   get '/' do
     if request.env.has_key?('X_MOBILE_DEVICE')
-        File.read(File.join('./', 'index2.html'))
+        File.read(File.join('./', 'mobile_index.html'))
     else
         File.read(File.join('./', 'index.html'))
     end
